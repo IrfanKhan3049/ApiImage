bbf5f72561dc92d294cf440b6478c0d6a267ee55
apple <apple@apples-MacBook-Pro-2.local>
2020-02-18 11:36:10 +0600

    ImageSHow From Api

---
+++ Irfan_ImageFromApi/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  Irfan_ImageFromApi
+//
+//  Created by apple on 2/17/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Irfan_ImageFromApi/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Irfan_ImageFromApi
+//
+//  Created by apple on 2/17/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+import  Alamofire
+import  AlamofireImage
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var imageName = [String]()
+    var modelData = [MyModel]()
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    func loadData() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/photos")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error == nil {
+                do{
+                    let myData = try! JSONDecoder().decode([MyModel].self, from: data!)
+                    DispatchQueue.main.async {
+                        for n in myData{
+                            
+                            self.imageName.append(n.url)
+                        }
+                        
+                        self.tableview.reloadData()
+                    }
+                }catch{
+                    print("nothing")
+                }
+                
+            }
+            
+        }.resume()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+                let url = URL(string: imageName[indexPath.row])
+                       DispatchQueue.global().async {
+                           //let data = try?Data(contentsOf: url!)
+                           DispatchQueue.main.async {
+                               //withAlamofireImageloadingWithurl;
+                             cell.cellImageView.af_setImage(withURL: url!)
+                               
+                               //cell.cellImageView.image = UIImage(data: data!)
+                           }
+                           
+                       }
+                return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadData()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
